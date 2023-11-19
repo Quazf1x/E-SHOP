1746e24381ee03a84acfe3ff1142d90336339ca1
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2023-11-19 22:00:17 +0600

    Ver 0.0.1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./assets/components/header/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/components/header/header.tsx
@@ -0,0 +1,26 @@
+function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <div />
+        <h1> E-SHOP </h1>
+      </div>
+
+      <nav>
+        <ul className="link-menu">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Browse</a>
+          </li>
+          <li>
+            <a>Checkout</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
