0e21f5aa5625ce39e1f1801f20749a0e5056e1b1
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2023-12-23 20:33:23 +0600

    Ver 0.1.4

--- src/assets/components/BrowsePage/FilterBar.tsx
+++ src/assets/components/BrowsePage/FilterBar.tsx
@@ -1,5 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faWindows,
+  faApple,
+  faXbox,
+  faPlaystation,
+} from "@fortawesome/free-brands-svg-icons";
+
 const FilterBar = () => {
-  return <div className="filter-bar-wrapper">hiiiiii</div>;
+  return (
+    <div className="filter-bar-wrapper">
+      <div>
+        <h3 className="filter-heading">System</h3>
+        <FontAwesomeIcon icon={faWindows} />
+        <FontAwesomeIcon icon={faApple} />
+        <FontAwesomeIcon icon={faXbox} />
+        <FontAwesomeIcon icon={faPlaystation} />
+      </div>
+    </div>
+  );
 };
 
 export default FilterBar;
