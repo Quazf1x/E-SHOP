5dd7f5ae7e175aada4d2198a62f7232dd075ea0c
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2024-01-03 15:35:55 +0600

    Ver 0.1.10

--- src/assets/API/FetchData.ts
+++ src/assets/API/FetchData.ts
@@ -1,8 +1,10 @@
+const URL = "https://api.rawg.io/api";
+const KEY = "911a250b30c54e34a60dcbcf35aba8dd";
+
 const fetchData = async (pageNum: number) => {
-  const response = await fetch(
-    `https://api.rawg.io/api/games?page=${pageNum}&key=911a250b30c54e34a60dcbcf35aba8dd`,
-    { mode: "cors" },
-  );
+  const response = await fetch(`${URL}/games?page=${pageNum}&key=${KEY}`, {
+    mode: "cors",
+  });
   if (!response.ok) throw new Error("something went wrong!");
 
   const data = await response.json();
