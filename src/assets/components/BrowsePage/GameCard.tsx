8d54d46780d4d932f9b76e284f3d7d11d34257dd
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2024-01-04 19:15:01 +0600

    Ver 0.1.14

--- src/assets/components/BrowsePage/GameCard.tsx
+++ src/assets/components/BrowsePage/GameCard.tsx
@@ -1,5 +1,9 @@
 const GameCard = () => {
-  return <div className="gamecard">game</div>;
+  return (
+    <div className="gamecard">
+      <img src="https://media.rawg.io/media/games/7fa/7fa0b586293c5861ee32490e953a4996.jpg" />
+    </div>
+  );
 };
 
 export default GameCard;
