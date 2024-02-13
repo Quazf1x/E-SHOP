a41573162f53cadf1cd8aea62a0b81d3777c53ce
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2024-02-13 21:49:17 +0600

    Ver 0.3.4.8

--- src/assets/components/BrowsePage/FilterBar.tsx
+++ src/assets/components/BrowsePage/FilterBar.tsx
@@ -6,18 +6,65 @@ import {
   faXbox,
   faPlaystation,
 } from "@fortawesome/free-brands-svg-icons";
+import { useSearchParams } from "react-router-dom";
 
 import SystemCheckbox from "./SystemCheckbox";
+import { useState } from "react";
 
 const FilterBar = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [currSystems, setCurrSystems] = useState<string[]>([]);
+
+  const onCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const system: string = e.target.dataset.system;
+    const checked = e.target.checked;
+    // const newSearchParam =
+    //   searchParams.get("system") == undefined
+    //     ? system
+    //     : searchParams.get("system")?.concat(",", system);
+    if (checked) {
+      const _currSystems: string[] = currSystems.push(system);
+      setCurrSystems(_currSystems);
+      //   setSearchParams((params) => {
+      //   params.set("system", newSearchParam);
+      //   return params;
+      // });
+    } else {
+      // setSearchParams((params) => {
+      //   params.delete("system", newSearchParam.replace("," + system, ""));
+      //   return params;
+      // });
+    }
+    setSearchParams((params) => {
+      params.set("system", currSystems.join(""));
+      return params;
+    });
+  };
+
   return (
     <div className="filter-bar-wrapper">
       <div>
         <h3 className="browse-heading">System</h3>
-        <SystemCheckbox system="windows" id="windows-check" icon={faWindows} />
-        <SystemCheckbox system="apple" id="apple-check" icon={faApple} />
-        <SystemCheckbox system="xbox" id="xbox-check" icon={faXbox} />
         <SystemCheckbox
+          onCheck={onCheck}
+          system="windows"
+          id="windows-check"
+          icon={faWindows}
+        />
+        <SystemCheckbox
+          onCheck={onCheck}
+          system="apple"
+          id="apple-check"
+          icon={faApple}
+        />
+        <SystemCheckbox
+          onCheck={onCheck}
+          system="xbox"
+          id="xbox-check"
+          icon={faXbox}
+        />
+        <SystemCheckbox
+          onCheck={onCheck}
           system="PS5"
           id="playstation-check"
           icon={faPlaystation}
--- src/assets/components/BrowsePage/SystemCheckbox.tsx
+++ src/assets/components/BrowsePage/SystemCheckbox.tsx
@@ -1,45 +1,21 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
-import { useState } from "react";
-import { useSearchParams, useNavigate } from "react-router-dom";
 
 type SystemCheckboxTypes = {
   id: string;
   system: string;
   icon: IconDefinition;
+  onCheck: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const SystemCheckbox = ({ id, system, icon }: SystemCheckboxTypes) => {
-  //const [checked, setChecked] = useState(false);
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const onCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const checked = e.target.checked;
-    const newSearchParam =
-      searchParams.get("system") == undefined
-        ? system
-        : searchParams.get("system")?.concat(",", system);
-    if (checked) {
-      setSearchParams((params) => {
-        params.set("system", newSearchParam);
-        return params;
-      });
-    } else {
-      setSearchParams((params) => {
-        params.delete("system", newSearchParam.replace("," + system, ""));
-        return params;
-      });
-    }
-    console.log(newSearchParam.replace(system, ""));
-  };
-
+const SystemCheckbox = ({ id, system, icon, onCheck }: SystemCheckboxTypes) => {
   return (
     <label className="system-checkbox-label" htmlFor={id}>
       <input
         id={id}
         className="system-checkbox"
         type="checkbox"
-        //checked={checked}
+        data-system={system}
         onChange={onCheck}
       />
       <FontAwesomeIcon icon={icon} />
