15a334f1e15f1abef4245616863ab0e0ab446008
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2024-03-03 15:31:19 +0600

    Ver 0.4.1

--- src/assets/components/BrowsePage/GameCard.tsx
+++ src/assets/components/BrowsePage/GameCard.tsx
@@ -11,16 +11,7 @@ const GameCard = ({ bgImg, gameName, id }: gameTypes) => {
 
   return (
     <div className="gamecard">
-      <img
-        style={{
-          backgroundColor: "var(--lighter-bg)",
-          display: "flex",
-          textAlign: "center",
-          lineHeight: "400px",
-        }}
-        src={bgImg}
-        alt={gameName}
-      />
+      <img src={bgImg} alt={gameName} />
       <div className="gamecard-info-wrapper">
         <h4 className="gamecard-name">{gameName}</h4>
         <div>
