3e02339afdcdc9ccdc7f9df7c52f2c71040da7e3
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2024-03-20 11:39:10 +0600

    Ver 0.4.7.5

--- src/assets/components/GamePage/GameInfo.tsx
+++ src/assets/components/GamePage/GameInfo.tsx
@@ -1,16 +1,31 @@
-const GameInfo = ({ gameDetails }) => {
+const GameInfo = ({ gameDetails }: GameDetails) => {
   console.log(gameDetails);
+  const releaseDate = gameDetails.released.replaceAll("-", ".");
+  const devNames = gameDetails.developers.map((dev) => dev.name + ", ");
+  console.log(devNames);
   return (
     <div className="gamepage-infoblock">
       <p className="gamepage-description">{gameDetails.description_raw}</p>
 
       <div className="gamepage-details">
-        <p>Released: {gameDetails.released.replaceAll("-", ".")}</p>
-        <p>Developers: {gameDetails.developers[0].name}</p>
+        <p>Released: {releaseDate}</p>
+        <p>Developers: {devNames}</p>
         <button className="gamepage-more-btn">More</button>
       </div>
     </div>
   );
+};
+
+type GameDetails = {
+  gameDetails: {
+    description_raw: string;
+    released: string;
+    developers: GameDeveloper[];
+  };
+};
+
+type GameDeveloper = {
+  name: string;
 };
 
 export default GameInfo;
