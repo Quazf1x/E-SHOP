9d1b80cfa2b079d1f3dc019989b5fa5e101263a5
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2024-05-14 16:23:26 +0600

    Ver 1.0.5

--- src/assets/components/CheckoutPage/CheckoutPage.tsx
+++ src/assets/components/CheckoutPage/CheckoutPage.tsx
@@ -2,7 +2,7 @@ import Header from "../Header/Header.tsx";
 import OrderedGame from "./OrderedGame.tsx";
 import OrderType from "./OrderType.ts";
 import { CartContext } from "../Providers/CartProvider.tsx";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useMemo } from "react";
 
 const CheckoutPage = () => {
   const { cartList } = useContext(CartContext);
@@ -26,8 +26,12 @@ const CheckoutPage = () => {
     localStorage.setItem("cartList", JSON.stringify(cartList));
   }, [cartList]);
 
-  let totalPrice = 0;
-  cartList.forEach((order: OrderType) => (totalPrice += order.price));
+  const totalPrice = useMemo(() => {
+    return cartList.reduce(
+      (totalSum, currentProduct) => totalSum + currentProduct.price,
+      0,
+    );
+  }, [cartList]);
 
   return (
     <>
